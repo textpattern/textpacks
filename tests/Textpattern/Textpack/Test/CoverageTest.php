ee5e5eab51dc1c54f8854a64e6a092598e5628e3
Jukka Svahn <web@rahinaa.biz>
2013-08-15 16:59:43 +0300

    Reduce memory usage and reads.

--- tests/Textpattern/Textpack/Test/CoverageTest.php
+++ tests/Textpattern/Textpack/Test/CoverageTest.php
@@ -5,35 +5,41 @@ use Textpattern\Textpack\Test\Parser as Textpack;
 
 class CoverageTest extends \PHPUnit_Framework_TestCase
 {
-    private $defaultTextpack;
-    private $textpack;
-    private $translations;
-    private $knownStrings = array();
+    static private $defaultTextpack;
+    static private $textpack;
+    static private $translations;
+    static private $knownStrings = array();
+    static private $setUp = false;
 
     public function setUp()
     {
-        $this->textpack = new Textpack();
-        $contents = file_get_contents(__DIR__.'/../../../../textpacks/en-gb.textpack');
-        $this->defaultTextpack = $this->textpack->parse($contents);
-        $this->translations = new TextpackFilter(new \DirectoryIterator(__DIR__.'/../../../../textpacks'));
-
-        foreach ($this->defaultTextpack as $data)
+        if (self::$setUp === false)
         {
-            $this->knownStrings[$data['name']] = $data['name'];
+            self::$textpack = new Textpack();
+            $contents = file_get_contents(__DIR__.'/../../../../textpacks/en-gb.textpack');
+            self::$defaultTextpack = self::$textpack->parse($contents);
+            self::$translations = new TextpackFilter(new \DirectoryIterator(__DIR__.'/../../../../textpacks'));
+
+            foreach (self::$defaultTextpack as $data)
+            {
+                self::$knownStrings[$data['name']] = $data['name'];
+            }
+
+            self::$setUp = true;
         }
     }
 
     public function testDefaultTextpack()
     {
-        $this->assertTrue(is_array($this->defaultTextpack));
-        $this->assertGreaterThan(1, count($this->defaultTextpack));
+        $this->assertTrue(is_array(self::$defaultTextpack));
+        $this->assertGreaterThan(1, count(self::$defaultTextpack));
     }
 
     public function testDuplicates()
     {
         $duplicate = $used = array();
 
-        foreach ($this->defaultTextpack as $data)
+        foreach (self::$defaultTextpack as $data)
         {
             if (in_array($data['name'], $used))
             {
@@ -50,7 +56,7 @@ class CoverageTest extends \PHPUnit_Framework_TestCase
 
     public function testUTF8Encoding()
     {
-        foreach ($this->translations as $file)
+        foreach (self::$translations as $file)
         {
             $this->assertTrue(mb_check_encoding(file_get_contents($file->getPathname()), 'UTF-8'), $file->getBasename());
         }
@@ -58,7 +64,7 @@ class CoverageTest extends \PHPUnit_Framework_TestCase
 
     public function testTrailingWhitespace()
     {
-        foreach ($this->translations as $file)
+        foreach (self::$translations as $file)
         {
             $contents = file_get_contents($file->getPathname());
             $this->assertTrue(trim($contents) === $contents, $file->getBasename());
@@ -67,11 +73,11 @@ class CoverageTest extends \PHPUnit_Framework_TestCase
 
     public function testMisingStrings()
     {
-        foreach ($this->translations as $file)
+        foreach (self::$translations as $file)
         {
             $contents = file_get_contents($file->getPathname());
-            $strings = $this->textpack->parse($contents);
-            $missing = $this->knownStrings;
+            $strings = self::$textpack->parse($contents);
+            $missing = self::$knownStrings;
 
             foreach ($strings as $data)
             {
@@ -84,32 +90,32 @@ class CoverageTest extends \PHPUnit_Framework_TestCase
 
     public function testWidowStrings()
     {
-        foreach ($this->translations as $file)
+        foreach (self::$translations as $file)
         {
             $contents = file_get_contents($file->getPathname());
-            $strings = $this->textpack->parse($contents);
+            $strings = self::$textpack->parse($contents);
 
             foreach ($strings as $data)
             {
-                $this->assertArrayHasKey($data['name'], $this->knownStrings, 'string '.$data['name'].' in '.$file->getBasename().' is not in en-gb');
+                $this->assertArrayHasKey($data['name'], self::$knownStrings, 'string '.$data['name'].' in '.$file->getBasename().' is not in en-gb');
             }
         }
     }
 
     public function testStringOrder()
     {
-        foreach ($this->translations as $file)
+        foreach (self::$translations as $file)
         {
             $contents = file_get_contents($file->getPathname());
-            $strings = $this->textpack->parse($contents);
+            $strings = self::$textpack->parse($contents);
 
             foreach ($strings as $key => $data)
             {
                 $this->assertTrue(
-                    $this->defaultTextpack[$key]['name'] === $data['name'] &&
-                    $this->defaultTextpack[$key]['event'] === $data['event'] &&
-                    $this->defaultTextpack[$key]['owner'] === $data['owner'],
-                    'Index '.$key.' in '.$file->getBasename().' does not match en-gb: '.$data['name'].' vs. '.$this->defaultTextpack[$key]['name']
+                    self::$defaultTextpack[$key]['name'] === $data['name'] &&
+                    self::$defaultTextpack[$key]['event'] === $data['event'] &&
+                    self::$defaultTextpack[$key]['owner'] === $data['owner'],
+                    'Index '.$key.' in '.$file->getBasename().' does not match en-gb: '.$data['name'].' vs. '.self::$defaultTextpack[$key]['name']
                 );
             }
         }
