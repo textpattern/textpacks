eea7eef6436c531d7f70f8338d94f0533b330e6b
Jukka Svahn <web@rahinaa.biz>
2013-08-15 19:46:57 +0300

    Travis doesn't truly need to run composer install.
    Instead just include the files we need. Will put
    autoloader back if its ever needed.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,5 @@
 <?php
 
 error_reporting(E_ALL);
-
-include dirname(__DIR__) . '/vendor/autoload.php';
-
-$loader = new \Composer\Autoload\ClassLoader();
-$loader->add('Textpattern\Textpack\Test', __DIR__);
-$loader->register();
+include __DIR__ . '/Textpattern/Textpack/Test/Parser.php';
+include __DIR__ . '/Textpattern/Textpack/Test/TextpackFilter.php';
